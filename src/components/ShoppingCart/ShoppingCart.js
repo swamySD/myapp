6295a14dd3bac39d0c56afb0f2ad00a808291b37
Swamy <swamykumar539@gmail.com>
2022-11-29 15:32:06 +0530

    g

---
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ShoppingCart = () => {
+  return (
+    <div className='container-fluid'>
+        <div className='row'>
+            <div className='col-12 col-lg-8'>
+                <h1>Heading</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ShoppingCart
